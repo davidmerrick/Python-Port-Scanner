eaae5f8accd56d3175bdca9abd0b6c67dbf7ddec
david <david@hal.davidmerrick.local>
2013-03-27 22:50:12 -0700

    Added function for port scanning. Imported thread library.

--- port_scanner.py
+++ port_scanner.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
 from socket import * 
+import threading
+
+
+def scan_port_range(min, max, target_ip):
+	for i in range(min, max):
+		s = socket(AF_INET, SOCK_STREAM)
+		result = s.connect_ex((target_ip, i))
+		
+		if(result == 0):
+			print 'Port %d: OPEN' % (i,)
+		s.close()
+
+
 
 if __name__ == '__main__':
-    target = raw_input('Enter host to scan: ')
-    targetIP = gethostbyname(target)
-    print 'Starting scan on host ', targetIP
+    	target = raw_input('Enter host to scan: ')
+    	targetIP = gethostbyname(target)
+   	 print 'Starting scan on host ', targetIP
 
-    #scan reserved ports
-    for i in range(20, 1025):
-        s = socket(AF_INET, SOCK_STREAM)
-
-        result = s.connect_ex((targetIP, i))
-
-        if(result == 0) :
-            print 'Port %d: OPEN' % (i,)
-        s.close()
+    	#scan reserved ports
+   	for i in range(20, 1025, 100):
+		
