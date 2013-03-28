d1773285e20171ae8c49e56302ed066668430293
david <david@hal.davidmerrick.local>
2013-03-27 23:25:17 -0700

    Added threading

--- port_scanner.py
+++ port_scanner.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 from socket import * 
 import threading
+import sys
 
+#Global variables
+num_threads = 100
 
 def scan_port_range(min, max, target_ip):
 	for i in range(min, max):
@@ -12,13 +15,19 @@ def scan_port_range(min, max, target_ip):
 			print 'Port %d: OPEN' % (i,)
 		s.close()
 
-
-
 if __name__ == '__main__':
-    	target = raw_input('Enter host to scan: ')
-    	targetIP = gethostbyname(target)
-   	 print 'Starting scan on host ', targetIP
+	max_port = 1025 #Highest port to scan
+	target = sys.argv[1]
+	target_ip = gethostbyname(target)
+   	print 'Starting scan on host ', target_ip
 
-    	#scan reserved ports
-   	for i in range(20, 1025, 100):
-		
+    	#scan reserved ports 20-max_port
+	#Special case: first min must start at 20
+	scan_port_range(20, max_port / num_threads, target_ip)
+   	for i in range(1, num_threads):
+		#Determine next min and max port ranges to scan
+		min = i * (max_port / num_threads) + 1
+		max = max_port if (i == num_threads - 1) else min + (max_port / num_threads) - 1
+		#Spawn a new thread
+		t = threading.Thread(target=scan_port_range, args=(min, max, target_ip))
+		t.start()
