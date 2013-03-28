a8fb0ec5bd00e3b7db0330fbb26abe36639ca71c
david <david@hal.davidmerrick.local>
2013-03-28 00:01:13 -0700

    Added synchronization mechanisms to lock access to array of open ports

--- port_scanner.py
+++ port_scanner.py
@@ -5,22 +5,32 @@ import sys
 
 #Global variables
 num_threads = 100
+open_ports = []
+lock = threading.Lock()
+
 
 def scan_port_range(min, max, target_ip):
 	for i in range(min, max):
 		s = socket(AF_INET, SOCK_STREAM)
 		result = s.connect_ex((target_ip, i))
 		
-		#Print results	
+		#Add any open ports to the array	
 		if(result == 0):
-			print 'Port %d: OPEN' % (i,)
+			lock.acquire() #Lock the array from access by other threads
+			open_ports.append(i)
+			lock.release() 
 		s.close()
+
+def print_results():
+	open_ports.sort()
+	for i in open_ports:
+		print 'Port %d: OPEN' % (i,)
 
 if __name__ == '__main__':
 	max_port = 1025 #Highest port to scan
 	target = sys.argv[1]
 	target_ip = gethostbyname(target)
-   	print 'Starting scan on host ', target_ip
+   	print 'Starting scan on host:', target_ip
 
     	#scan reserved ports 20-max_port
    	for i in range(0, num_threads):
@@ -30,3 +40,5 @@ if __name__ == '__main__':
 		#Spawn a new thread
 		t = threading.Thread(target=scan_port_range, args=(min, max, target_ip))
 		t.start()
+	
+	print_results()
